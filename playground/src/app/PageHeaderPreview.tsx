e2c61f260f320d884b8dead8c65649a685be01b8
kakeru-ikeda <kakerumail1451@gmail.com>
2026-04-22 00:22:14 +0900

    feat: AvatarPreview と PageHeaderPreview のコードフォーマットを整形

--- playground/src/app/PageHeaderPreview.tsx
+++ playground/src/app/PageHeaderPreview.tsx
@@ -1,11 +1,11 @@
 import { PageHeader, PageHeaderTitle } from "../../../src/System/PageHeader";
 
 export function PageHeaderPreview() {
-  return (
-    <div className="flex w-full max-w-md flex-col items-center gap-3">
-      <PageHeader>
-        <PageHeaderTitle>Page Title</PageHeaderTitle>
-      </PageHeader>
-    </div>
-  );
+	return (
+		<div className="flex w-full max-w-md flex-col items-center gap-3">
+			<PageHeader>
+				<PageHeaderTitle>Page Title</PageHeaderTitle>
+			</PageHeader>
+		</div>
+	);
 }
